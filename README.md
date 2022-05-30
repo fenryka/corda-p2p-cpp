db6acee8626c9e3d917316ee00526217dcb8b904
Kat Baker <katelyn.baker@r3.com>
2022-05-30 08:31:09 +0100

    Initial Commit - Nothing works as portingf from C

---
+++ README.md
@@ -0,0 +1,11 @@
+brew install librdkafka
+
+If you need to have openssl@1.1 first in your PATH run:
+  echo 'export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"' >> ~/.zshrc
+
+For compilers to find openssl@1.1 you may need to set:
+  export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+  export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
+
+For pkg-config to find openssl@1.1 you may need to set:
+  export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
---
+++ common.cxx
@@ -0,0 +1,34 @@
+#include <glib.h>
+#include <librdkafka/rdkafkacpp.h>
+
+static void
+load_config_group(
+        RdKafka::Conf & conf,
+        GKeyFile *key_file,
+        const char *group
+) {
+    g_autoptr(GError) error = nullptr;
+
+    gchar **ptr = g_key_file_get_keys(key_file, group, nullptr, &error);
+    if (error) {
+        g_error("%s", error->message);
+    }
+
+    while (*ptr) {
+        const char *key = *ptr;
+        g_autofree gchar *value = g_key_file_get_string(key_file, group, key, &error);
+
+        if (error) {
+            g_error("Reading key: %s", error->message);
+        }
+
+        std::string err;
+        auto result = conf.set(std::string (key), std::string (value), err);
+
+        if (result != RdKafka::Conf::ConfResult::CONF_OK) {
+            throw std::runtime_error (err);
+        }
+
+        ptr++;
+    }
+}
